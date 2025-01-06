2081fa57dc171733e02621274abcf26a8a21463e
tanzid64 <tanzid3@gmail.com>
2025-01-06 22:49:39 +0600

    feat: Email settings

--- core/settings.py
+++ core/settings.py
@@ -138,3 +138,11 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
 } 
+
+# Email Configurations
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
